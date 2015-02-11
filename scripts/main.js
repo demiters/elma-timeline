8c234cd77bd9250c617fcfe7529330f166b25579
Arturs Demiters <arturs.demiters@gmail.com>
2015-02-11 07:42:49 +0200

    minor adjustment and borrowed a favicon

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     source: 'https://docs.google.com/spreadsheet/pub?key=0AldnkYMPn1tydFVzX3lCUjR2eGtqMjZVZlMwT3pYZHc&output=html',
     embed_id: 'elma-timeline',
     // Reverse order of display
-    start_at_end: false,
+    start_at_end: true,
     start_at_slide: '0',
     start_zoom_adjust: '0',
     // Allow bookmarking slides using the hashtag
